73027ddbe48c98825ca353560b37df7878cb5a9f
Godson2611 <godson2611@gmail.com>
2023-12-02 03:24:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import express from "express";
+import AppRouter from "./routers/router.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = Number(process.env.PORT);
+
+app.use(express.json());
+app.use("/", AppRouter);
+
+mongoose
+  .connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
